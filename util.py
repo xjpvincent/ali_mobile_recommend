fae232c5d4f719c985243f5d80942f7bb7bee16f
xujingping <xujingping@mail.bnu.edu.cn>
2015-03-30 23:34:33 +0800

    update util.py

--- util.py
+++ util.py
@@ -6,6 +6,7 @@ Created on Sat Mar 28 19:38:08 2015
 """
 
 import pandas as pd
+import numpy as np
 
 
 def list2csv(li,filename):
@@ -53,25 +54,27 @@ def evaluate_model(test_label_file,predict_label_file):
     return result
     
     
-def extract_itemfeature(user_file,filename="feature_item.csv"):
+def extract_itemfeature(train_data_file,item_file,feature_file="feature_user.csv"):
+    train_data=pd.read_csv(train_data_file)
+    item_data=pd.read_csv(item_file)
+    feature_item=pd.DataFrame({'item_id':item_data.item_id.unique()})
+    
+    #
+    feature_item=item_buynums(train_data,feature_item)
+    
+    
     #load data
-    raw_data=pd.read_table(user_file)
-    #buy_numbers
-    buy    =raw_data[raw_data['behavior_type']==4]
-    target_item = pd.read_table('tianchi_mobile_recommend_train_item.csv',sep=',')
-    #buy_numbers
-    buy_target_item=pd.merge(target_item, buy, how='inner', on='item_id', left_on=None, right_on=None, left_index=False, right_index=False, sort=False, suffixes=('_x', '_y'), copy=True) 
-    item_buy_num = buy_target_item['item_id'].value_counts()
-    item_buy_num.to_csv(filename)   
+        #保存
+    feature_item.to_csv(feature_file,index=False)
 
+  
 	
 def extract_userfeature(train_file,feature_file="feature_user.csv"):
     """
       Input:
           user_file:
       Output:file  
-    """
-    
+    """    
     #load data      
     feature_user=pd.read_csv(feature_file)
     train_data=pd.read_csv(train_file)
@@ -93,30 +96,52 @@ def extract_userfeature(train_file,feature_file="feature_user.csv"):
 
     #保存
     feature_user.to_csv(feature_file,index=False)
+
+def extract_useritemfeature(train_file="train_data.csv",feature_file="feature_useritem.csv"):
+    #load data
+    df=pd.read_csv(train_file)
+    ui=df.groupby(['user_id','item_id']).sum()
+    ui=ui.index.tolist()
+    li_user=[]
+    li_item=[]
+    for tmp in ui:
+        li_user.append(tmp[0])
+        li_item.append(tmp[1])
+    feature_useritem=pd.DataFrame({'user_id':li_user,'item_id':li_item})
+    feature_useritem['key']=ui    
+    #cart7
+    ui=df.loc[:,['user_id','item_id','behavior_type']]   
+    ui=ui[ui.behavior_type>1]
+    
+    f1=ui.groupby(['user_id','item_id']).mean()
+    f1['key']=f1.index.tolist()    
+    feature_useritem=pd.merge(feature_useritem,f1,how='outer',on='key')
+    #save data
+    feature_useritem.to_csv(feature_file,index=False)
     
     
-    
-def merge_feature(userfeature_file="feature_user.csv",itemfeature_file="feature_item.csv",useritemfeature_file="feature_useritem.csv",feature_file="featues_train.csv"):
+def merge_feature(userfeature_file="feature_user.csv",itemfeature_file="feature_item.csv",label_file="train_label.csv",useritemfeature_file="feature_useritem.csv",feature_file="featues_train.csv"):
     #load data
     user_feature=pd.read_csv(userfeature_file)
     item_feature=pd.read_csv(itemfeature_file)
     useritem_feature=pd.read_csv(useritemfeature_file)
+    train_label=pd.read_csv(label_file)
     #merge
-    useritem_feature=pd.merge(useritem_feature,user_feature)
-    useritem_feature=pd.merge(useritem_feature,item_feature)
-    
+    useritem_feature=pd.merge(useritem_feature,user_feature,how='outer',on='user_id')
+    useritem_feature=pd.merge(useritem_feature,item_feature,how='outer',on='item_id')
+    useritem_feature=pd.merge(useritem_feature,train_label,how='outer',on=['user_id','item_id'])
     #save 
-    useritem_feature.to_cvs(feature_file)  
+    useritem_feature=useritem_feature.fillna(0)
+    useritem_feature.to_cvs(feature_file,index=False)  
     
+#cut the train data from date_cut    
 def cut_traindata(traindata_file,date_cut,pre_traindata,post_traindata):
     #load data
     traindata=pd.read_csv(traindata_file)
-    pre=traindata[traindata.date<date_cut]
-    post=traindata[traindata.date>=date_cut]
-    pre.to_csv(pre_traindata)
-    post.to_csv(post_traindata)
-    
-    
+    pre=traindata[traindata.time<date_cut]
+    post=traindata[traindata.time>=date_cut]
+    pre.to_csv(pre_traindata,index=False)
+    post.to_csv(post_traindata,index=False)
     
 """
 Defined the subfunction in this section 
@@ -135,7 +160,29 @@ def sample_item(item_file,sample_item):
     li=user_id.tolist()
     df=pd.DataFrame({'item_id':li})
     df.to_csv(sample_user,index=False)    
-    
+
+
+
+def sample_ui(user_file,item_file,sample_user_item):
+    df1=pd.read_csv(user_file)
+    df2=pd.read_csv(item_file)
+    user_id=df1.user_id.unique().tolist()
+    del df1
+    item_id=df2.item_id.unique().tolist()
+    del df2
+    user_id_extend=[]
+    for i in range(len(item_id)):
+        user_id_extend.extend(user_id)
+    print 'user_id'
+
+    item_id_extend=[]
+    for i in range(len(user_id)):
+        item_id_extend.extend(item_id)
+        
+    user_item=pd.DataFrame({'user_id':user_id_extend,'item_id':item_id_extend})       
+    del user_item
+    del user_id
+    del item_id
     
 
 ##############################################################################
@@ -194,35 +241,62 @@ def user_shoucangnums(user_data,feature_user):
     feature_user.fillna(0)
     return feature_user
 
+##item features ##  
+def item_buynums(user_data,feature_item):
+    item_buy=user_data.loc[:,['item_id','behavior_type']]
+    item_buy=item_buy[item_buy.behavior_type==4]
+    item_buy.behavior_type=1
+    item_buy=item_buy.groupby("item_id").sum()
+    item_buy['item_id']=item_buy.index
+    item_buy['buyed_nums']=item_buy.behavior_type
+#    item_buy=item_buy.loc[:,['user_id','buy_num']]
+    del item_buy['behavior_type']
+    filter_item=pd.DataFrame({'item_id':feature_item.item_id})
+    #filter
+    item_buy=pd.merge(filter_item,item_buy,how='inner',on='item_id')
+    feature_item=pd.merge(feature_item,item_buy,how='outer',on=None) 
+    feature_item.fillna(0)
+    return feature_item
 # 提取ylabel
-def extract_ylabel(user_id_file,item_id_file,train_data_file,lable_file):
-    user_id=pd.read_csv(user_id_file)
-    item_id=pd.read_csv(item_id_file)
+def extract_ylabel(train_data_file,item_id_file,lable_file):
     train_data=pd.read_csv(train_data_file)
+    item_id=pd.read_csv(item_id_file)
+    item_filter=pd.DataFrame({'item_id':item_id.item_id.unique()})
     train_data=train_data[train_data.behavior_type==4]
-    label=train_data.loc[:,['user_id','item_id']]
-    ylabel=pd.merge(user_id,item_id)
-    ylabel=pd.merge(ylabel,label)
+    ylabel=train_data.loc[:,['user_id','item_id']]    
+    ylabel=pd.merge(ylabel,item_filter)
+    ylabel['label']=np.ones(len(ylabel))
     #save 
-    ylabel.to_csv(lable_file)
+    ylabel.to_csv(lable_file,index=False)
     #merge
-    
-def item_buynums(item_feature,item_data):
 
-    return item_feature
     
 ##############################################################################
 #test code    
 if __name__=="__main__":
-    sample_user('tianchi_mobile_recommend_train_user.csv','feature_user.csv')
-    extract_userfeature('train_data.csv')
+    
+    #split the train data
+    cut_traindata('tianchi_mobile_recommend_train_user.csv','2014-12-17','test_data.csv','test_data_lable.csv')
+    cut_traindata('test_data.csv','2014-12-16','train_data.csv','train_label_data.csv')
+    #extract user feature
+    extract_userfeature(train_file='train_data.csv',feature_file="feature_user.csv")
+    #extract item feature
+    extract_itemfeature(train_data_file='train_data.csv',item_file='tianchi_mobile_recommend_train_item.csv',feature_file="feature_item.csv")
+    #extract user_item feature
+    extract_useritemfeature(train_file="train_data.csv",feature_file="feature_useritem.csv")
+    #extract ylabel
+    extract_ylabel('train_data.csv','tianchi_mobile_recommend_train_item.csv','train_label.csv')
+    merge_feature(userfeature_file="feature_user.csv",itemfeature_file="feature_item.csv",label_file="train_label.csv",useritemfeature_file="feature_useritem.csv",feature_file="featues_train.csv")
+    #extract user feature
+
+    #sample_user('tianchi_mobile_recommend_train_user.csv','feature_user.csv')
+   # extract_userfeature('train_data.csv')
    # sample_user('train_data.csv','feature_user.csv')
+ 
     
     #test evaluate_model
    # print evaluate_model("test_label.csv","predict_label.csv")
    # sample_item('tianchi_mobile_recommend_train_item.csv','item_feature.csv')
 
-    
-    #
     
     
